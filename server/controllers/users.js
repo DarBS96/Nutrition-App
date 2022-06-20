7614fc0dfde086e0d7e56cff298b82a899952606
Dar Ben Shitrit <bsdar00@gmail.com>
2022-06-20 18:32:59 +0300

    login page

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,1 +1,12 @@
-const login = () => {};
+const path = require("path");
+const postLogin = (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+  res.json({ username, password });
+};
+
+const getLogin = (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/../../public/login/login.html"));
+};
+
+module.exports = { postLogin, getLogin };
