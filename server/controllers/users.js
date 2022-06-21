eddbc7bd5e4a99eb22108256a340453467a2162f
ortori <ortori@gmail.com>
2022-06-21 10:57:11 +0300

    .

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,8 +1,39 @@
 const path = require("path");
+const {
+  checkIfExist,
+  pushDataToDatabase,
+} = require("../../database/usersAuth");
 
-const PostRegister = (req, res) => {
-  console.log(req.body);
+const PostRegister = async (req, res) => {
+  const { username, email, password, firstName, lastName } = req.body;
+  const usernameExist = await checkIfExist({ ["username"]: username }, "users");
+  const emailExist = await checkIfExist({ ["email"]: email }, "users");
+  if (usernameExist) {
+    res.status(406).json({
+      msg: `The username '${username}' is already taken! Please choose a different one! ⛔`,
+    });
+    return;
+  } else if (emailExist) {
+    res.status(406).json({
+      msg: `The email '${email}' is already taken! Please choose a different one! ⛔`,
+    });
+    return;
+  } else {
+    pushDataToDatabase(
+      {
+        username,
+        password,
+        email,
+        first_name: firstName,
+        last_name: lastName,
+      },
+      "users"
+    );
+    res.status(200).send({ msg: `You have been registered successfully! 😀` });
+    return;
+  }
 };
+
 const GetRegister = (req, res) => {
   res.sendFile(
     path.resolve(__dirname + "/../../public/register/register.html")
@@ -10,7 +41,6 @@ const GetRegister = (req, res) => {
 };
 
 const postLogin = (req, res) => {
-  console.log(req.body);
   const { username, password } = req.body;
   res.json({ username, password });
 };
@@ -19,6 +49,4 @@ const getLogin = (req, res) => {
   res.sendFile(path.resolve(__dirname + "/../../public/login/login.html"));
 };
 
-module.exports = { postLogin, getLogin, PostRegister,
-  GetRegister, };
-
+module.exports = { postLogin, getLogin, PostRegister, GetRegister };
