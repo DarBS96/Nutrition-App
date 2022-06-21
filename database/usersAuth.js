6a3bdba8fee49884a4e6afd631633bede709ca83
ortori <ortori@gmail.com>
2022-06-21 10:57:11 +0300

    .

--- database/usersAuth.js
+++ database/usersAuth.js
@@ -5,7 +5,7 @@ const db = knex({
     host: "127.0.0.1",
     port: "5432",
     user: "postgres",
-    password: "Amit9496",
+    password: "Or2022",
     database: "nutritionApp",
   },
 });
@@ -19,20 +19,40 @@ const db = knex({
 //     console.log(err);
 //   });
 
-db("users")
-  .insert([
-    {
-      first_name: "Dar",
-      last_name: "Ben Shitrit",
-      email: "dsa@gmail.com",
-      username: "OrHachamod",
-      password: "123456",
-    },
-  ])
-  .returning("*")
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// db("users")
+//   .insert([
+//     {
+//       first_name: "Dar",
+//       last_name: "Ben Shitrit",
+//       email: "dsa@gmail.com",
+//       username: "OrHachamod",
+//       password: "123456",
+//     },
+//   ])
+//   .returning("*")
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const checkIfExist = (keyValueObj, table) => {
+  const isExist = db(table)
+    .select("*")
+    .where(keyValueObj)
+    .then((res) => !!res.length);
+  return isExist;
+};
+
+const pushDataToDatabase = (keyValueObj, table) => {
+  db(table)
+    .returning("*")
+    .insert([keyValueObj])
+    .then((res) => console.log(res));
+};
+
+module.exports = {
+  checkIfExist,
+  pushDataToDatabase,
+};
--- public/register/script.js
+++ public/register/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const form = document.forms.registerForm;
+const messageHeading = document.querySelector(".message");
 
 const register = (e) => {
   e.preventDefault();
@@ -12,16 +13,48 @@ const register = (e) => {
 };
 
 const sendDataToServer = async (dataObj, method, url) => {
-  const res = fetch(url, {
+  const res = await fetch(url, {
     method: method,
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(dataObj),
   });
+  const data = await res.json();
+  console.log(data);
+  return { status: res.status, msg: data.msg };
+};
+
+const showMessage = (color) => {
+  messageHeading.classList.remove("d-none");
+  messageHeading.classList.remove(`text-danger`);
+  messageHeading.classList.remove(`text-success`);
+  messageHeading.classList.add(`text-${color}`);
+};
+
+const renderErrorMessage = (message) => {
+  console.log(message);
+  showMessage("danger");
+  messageHeading.textContent = message;
+};
+
+const renderSuccessMessage = (message) => {
+  showMessage("success");
+  messageHeading.textContent = message;
 };
 
 form.addEventListener("submit", async (e) => {
   const formData = register(e);
-  sendDataToServer(formData, "POST", "/users/register");
+  const data = await sendDataToServer(formData, "POST", "/users/register");
+  if (data.status === 406) {
+    renderErrorMessage(data.msg);
+  } else if (data.status === 200) {
+    renderSuccessMessage(data.msg);
+    const spinner = document.querySelector(".spinner");
+    spinner.classList.remove("d-none");
+    form.classList.add("d-none");
+    setTimeout(() => {
+      location.href = "/users/login";
+    }, 5000);
+  }
 });
