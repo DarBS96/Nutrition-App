8dc128ae4714dbf7c7b32effc15930cf6fcb90a1
ortori <ortori@gmail.com>
2022-07-01 17:29:34 +0300

    .

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use("/", (req, res) => {
-  res.redirect("/");
+  res.redirect("/homepage");
 });
 app.use("/users", router);
 app.use("/homepage", routerHomePage);
